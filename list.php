439805c19b7a4014f9bb5fb77db3057604063c49
Mary Veghova <kispuding@gmail.com>
2015-10-05 15:01:52 +0200

    list.php
    just for testing

---
+++ list.php
@@ -0,0 +1,60 @@
+{
+"records": [
+  {
+    "Artist" : "Alfreds Futterkiste",
+    "Track" : "Berlin"
+  },
+  {
+    "Artist" : "Berglunds snabbköp",
+    "Track" : "Luleå"  },
+  {
+    "Artist" : "Centro comercial Moctezuma",
+    "Track" : "México D.F."  },
+  {
+    "Artist" : "Ernst Handel",
+    "Track" : "Graz"
+  },
+  {
+    "Artist" : "FISSA Fabrica Inter. Salchichas S.A.",
+    "Track" : "Madrid" },
+  {
+    "Artist" : "Galería del gastrónomo",
+    "Track" : "Barcelona" },
+  {
+    "Artist" : "Island Trading",
+    "Track" : "Cowes"
+  {
+    "Artist" : "Königlich Essen",
+    "Track" : "Brandenburg"
+  },
+  {
+    "Artist" : "Laughing Bacchus Wine Cellars",
+    "Track" : "Vancouver"  },
+  {
+    "Artist" : "Magazzini Alimentari Riuniti",
+    "Track" : "Bergamo" },
+  {
+    "Artist" : "North/South",
+    "Track" : "London"
+  {
+    "Artist" : "Paris spécialités",
+    "Track" : "Paris"  },
+  {
+    "Artist" : "Rattlesnake Canyon Grocery",
+    "Track" : "Albuquerque",
+  {
+    "Artist" : "Simons bistro",
+    "Track" : "København"
+  },
+  {
+    "Artist" : "The Big Cheese",
+    "Track" : "Portland",
+  {
+    "Artist" : "Vaffeljernet",
+    "Track" : "Århus"
+  },
+  {
+    "Artist" : "Wolski Zajazd",
+    "Track" : "Warszawa"  }
+]
+}
