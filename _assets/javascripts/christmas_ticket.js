9403572f6c862e45edfadb2723f1eb42049dbbc8
Szabolcs Balogh <baloghsz@szabi.org>
2015-12-18 12:48:17 +0100

    fix

--- _assets/javascripts/christmas_ticket.js
+++ _assets/javascripts/christmas_ticket.js
@@ -1,21 +1,25 @@
-if ($("form#christmas_ticket").length > 0) {
-  captcha_reload = function() { // reload captcha image
-    $('#christmas_ticket_captcha').css('background-image', 'url(/api/captcha?'+Date.now()+')');
-    $('#christmas_ticket_captcha').val("");
-  };
-
-  $('.re-captcha').click(captcha_reload);
-
-  $('#christmas_ticket').bind("keyup keypress", function(e) {
-    var code = e.keyCode || e.which;
-    if (code == 13) {
-      e.preventDefault();
-      return false;
+jQuery(document).ready(function($){
+  $(window).load(function() {
+    if ($("form#christmas_ticket").length > 0) {
+      captcha_reload = function() { // reload captcha image
+        $('#christmas_ticket_captcha').css('background-image', 'url(/api/captcha?'+Date.now()+')');
+        $('#christmas_ticket_captcha').val("");
+      };
+    
+      $('.re-captcha').click(captcha_reload);
+    
+      $('#christmas_ticket').bind("keyup keypress", function(e) {
+        var code = e.keyCode || e.which;
+        if (code == 13) {
+          e.preventDefault();
+          return false;
+        }
+      });
+    
+      $('#christmas_ticket').submit(function (e) {
+        console.log($("form#christmas_ticket").serializeObject());
+      });
+    
     }
   });
-
-  $('christmas_ticket').submit(function (e) {
-    console.log($("form#christmas_ticket").serializeObject());
-  });
-
-}
+});
