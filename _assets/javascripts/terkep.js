2e516fdb78a05757b50cf320548839e2a5bdc88d
Stubendek Attila <stubendek.attila@gmail.com>
2016-07-05 23:34:21 +0200

    map stub update 4

--- _assets/javascripts/terkep.js
+++ _assets/javascripts/terkep.js
@@ -4,12 +4,12 @@ if ($("#gomba-map").length > 0) {
 			{
 				id: 0,
 				area: {
-						xw1: 200,
-						xw2: 300,
-						yh1: 400,
-						yh2: 500
-					}
-				slug: test,
+						xw1: 940,
+						xw2: 982,
+						yh1: 1051,
+						yh2: 1093
+					},
+				slug: "test",
 				title: "Teszt",
 				program: false,
 				description: "Teszt cucc"
@@ -17,12 +17,12 @@ if ($("#gomba-map").length > 0) {
 			{
 				id: 1,
 				area: {
-						xw1: 400,
-						xw2: 500,
-						yh1: 500,
-						yh2: 600
-					}
-				slug: pajtaszinhaz,
+						xw1: 1491,
+						xw2: 310,
+						yh1: 1647,
+						yh2: 465
+					},
+				slug: "pajtaszinhaz",
 				title: "Pajta Színház",
 				program: true,
 				description: "Pajtaszinhaz"
@@ -31,18 +31,16 @@ if ($("#gomba-map").length > 0) {
 	
 	var imW = 1920;
 	var imH = 1348;
+	var imO = {};
 	
 	var setPosition = function(){
-		if (msie > 0)
-		{
+		imW = $("#gomba-map").width();
+		imH = $("#gomba-map").height();
+		if (imW == 0){
 			imW = $("#gomba-map").outerWidth();
 			imH = $("#gomba-map").outerHeight();
 		}
-		else
-		{
-			imW = $("#gomba-map").width();
-			imH = $("#gomba-map").height();
-		}
+		imO = $("#gomba-map").offset();
 	};
 	
 	var isThere = function(xw, yh, area){
@@ -50,17 +48,19 @@ if ($("#gomba-map").length > 0) {
 	};
 	
 	var getPlaceContent = function(px, py){
-		opxw = 1920*px/imW;
-		opyh = 1348*py/imH;
-		mapPlaces.forEach(place, function(){
+		opxw = 1920*(px-imO.left)/imW;
+		opyh = 1348*(py-imO.top)/imH;
+		console.log("OrigPosX: " + opxw + ", OrigPosY: " + opyh);
+		mapPlaces.forEach(function(place){
 				if(isThere(opxw, opyh, place.area)){
 					console.log("OrigPosX: " + opxw + ", OrigPosY: " + opyh + ", is in " + place.title);
 				}
 			});
 	};
 	
+	setPosition();
+	
 	$("#gomba-map").on("mousemove", function(e){
-			console.log("pageX: " + e.pageX + ", pageY: " + e.pageY);
 			getPlaceContent(e.pageX, e.pageY);
 		});
 }
