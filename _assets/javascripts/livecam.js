57bb5024e7d9779ebc43a3e22ea5410b6fbc1134
stubendek <stubendek.attila@gmail.com>
2017-02-14 00:38:00 +0100

    weather update

--- _assets/javascripts/livecam.js
+++ _assets/javascripts/livecam.js
@@ -6,7 +6,18 @@ if ($("#livepic").length > 0) {
 
 if ($("#weatherwrap").length > 0) {
 
-	function renderWeather() {
+	function fillZeros(st){
+		if((st.toString().length)<2){
+			return "0" + st;
+		}
+		return st;
+	}
+
+	function K2C(kelvin) {  
+		return Math.round(kelvin-273.15);
+	}  
+
+	function renderWeather(wData) {
 		var wIconWrap = document.getElementById("weatherimage");
 		while (wIconWrap.firstChild) {
 			wIconWrap.removeChild(wIconWrap.firstChild);
@@ -15,8 +26,16 @@ if ($("#weatherwrap").length > 0) {
 		imgNode.setAttribute("class", "weatherimage-image");
 		imgNode.setAttribute("src", "img/wms/" + wData.weather[0].icon + ".png");
 		wIconWrap.appendChild(imgNode);
+
 		var wTemp = document.getElementById("weathertemp");
 		wTemp.innerHTML = K2C(wData.main.temp) + " ℃";
+
+		var wPressure = document.getElementById("weatherpressure");
+		wPressure.innerHTML = "Légnyomás: " + wData.main.sea_level + " hPa";
+
+		var wHumid = document.getElementById("weatherhumidity");
+		wHumid.innerHTML = "Páratartalom: " + wData.main.humidity + "%";
+
 		var wSun = document.getElementById("weathersun");
 		var sunRise = new Date(parseInt(wData.sys.sunrise*1000));
 		// console.log("Napkelte: " + wData.sys.sunrise + ", " + (new Date(1476594047)));
@@ -24,7 +43,7 @@ if ($("#weatherwrap").length > 0) {
 		wSun.innerHTML = "Napkelte: " + fillZeros(sunRise.getHours()) + ":" + fillZeros(sunRise.getMinutes()) + ", naplemente: " + fillZeros(sunSet.getHours()) + ":" + fillZeros(sunSet.getMinutes());
 	}
 
-	setInterval(function() {
+	function updateWeather() {
 		var xmlhttp=new XMLHttpRequest();
 		var rnsr = Math.round(Math.random()*19182);
 		xmlhttp.open("GET", 'http://api.openweathermap.org/data/2.5/weather?lat=48.563488&lon=20.467229&appid=251c4ed706476f19a5e8569a4068df4d');
@@ -33,12 +52,19 @@ if ($("#weatherwrap").length > 0) {
 				if(xmlhttp.status === 200){
 					var wData = JSON.parse(xmlhttp.responseText);
 					renderWeather(wData);
+					console.log(wData.main);
 				}else{
 					console.log('News download error: ' + xmlhttp.statusText )
 				}
 			}
 		}
 		xmlhttp.send();
+	}
+
+	updateWeather();
+
+	setInterval(function() {
+		updateWeather();
 	}, 200000)
 }
 
