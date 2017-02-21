5c9cba6491843763ef33ca9d119682a1c9e90ca5
stubendek <stubendek.attila@gmail.com>
2017-02-21 09:37:03 +0100

    adding https to live image script

--- _assets/javascripts/livecam.js
+++ _assets/javascripts/livecam.js
@@ -1,6 +1,6 @@
 if ($("#livepic").length > 0) {
 	setInterval(function() {
-		$("#livepic").attr("src", "http://kadbudapest.hu/gombcam/igynezkimostakemping.jpg?"+new Date().getTime());
+		$("#livepic").attr("src", "https://kadbudapest.hu/gombcam/igynezkimostakemping.jpg?"+new Date().getTime());
 	}, 20000)
 }
 
