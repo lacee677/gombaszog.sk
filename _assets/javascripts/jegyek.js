28e63a18b4ca500254d6ec0a9995f77dd86f2c65
stubendek <stubendek.attila@gmail.com>
2017-12-04 18:02:22 +0100

    add data check fb

--- _assets/javascripts/jegyek.js
+++ _assets/javascripts/jegyek.js
@@ -99,7 +99,8 @@ if ($(".ticket-form").length > 0) {
             $('#ticket_last_name').val(profile.last_name);
             $('#ticket_city').val((profile.hometown ? profile.hometown.name : null));
             $('#ticket_where').val((profile.location ? profile.location.name : null));
-            date = profile.birthday.split("/").reverse(); // preparse date
+            if (profile.birthday)
+              date = profile.birthday.split("/").reverse(); // preparse date
             $('#ticket_birth').val([date[0],date[2],date[1]].join("-"));
             // show the form
             loadVars();
